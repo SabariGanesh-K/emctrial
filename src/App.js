a3ec24e8d3462c50ef184389de77b32353c15333
Sabari Ganesh <k.sabarii.ganesh@gmail.com>
2022-02-28 08:44:27 +0530

    final stable

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import fb from "./assets/facebook.png";
 function App() {
   return (
     <div className = "flex flex-col justify-between h-full">
+
     <div className="flex flex-row justify-between">
-     <div className = "flex flex-col justify-center">
+     <div className = "flex flex-col justify-center my-2 mx-2 text-center">
 <img src = {logo} alt = "logo"/>
-<p>Logo</p>
+<p className = "text-black font-semibold text-2xl">Logo</p>
      </div>
-     <div>
+     <div className = "my-3 mx-3">
      <label className="relative block">
   <span className="sr-only">Search</span>
  
@@ -22,25 +23,29 @@ function App() {
     </div>
 
 <div className = "flex flex-row justify-between my-10 w-full">
-  <div className = "flex flex-col justify-center w-1/4">
-    <div className = "text-black font-bold text-4xl">
+  <div className = "flex flex-col justify-center w-1/4 mx-10">
+    <div className = "text-black font-bold text-4xl my-1">
       SEE THE WORLD
     </div>
-    <div>
+    <div className = "my-1">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu
     </div>
-    <button>READ MORE</button>
+    <div className = "mx-1">
+    <button className = "bg-green-500 text-white font-normal w-3/6 rounded-lg py-1 my-1 ">READ MORE</button>
+    </div>
   </div>
-  <div className = "flex  flex-col justify-between">
+  <div className = "flex  flex-col justify-between my-2 mx-4">
     
-    <div>HOME</div>
- 
-    <div>PROFILE</div>
-    <div>CONTACT</div>
-    <div>GALLERY</div>
+    <div ><a href="#" className = " text-gray-400 text-3xl   hover:text-black hover:underline hover:decoration-3 hover:decoration-green-600">HOME</a></div>
+    <br/><br/>
+    <div ><a href="#" className = " text-gray-400 text-3xl  hover:text-black hover:underline hover:decoration-3 hover:decoration-green-600">PROFILE</a></div>
+    <br/><br/>
+    <div ><a href="#" className = " text-gray-400 text-3xl  hover:text-black hover:underline hover:decoration-3 hover:decoration-green-600">CONTACT</a></div>
+   <br/><br/>
+    <div ><a href="#" className = " text-gray-400 text-3xl   hover:text-black hover:underline hover:decoration-3 hover:decoration-green-600">GALLERY</a></div>
   </div>
 </div>
-
+<br/><br/>
 <div className = "flex flex-row justify-end ">
 <div className = "flex flex-end w-3/6 flex-row justify-between">
   <img src = {insta} alt = "insta"/>
